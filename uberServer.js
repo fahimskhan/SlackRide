35db521da425899a4604d03a1613035ddf79efa4
belgren <belgren28@gmail.com>
2018-08-03 15:34:18 -0700

    testing teletype

---
+++ uberServer.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+var Uber = require('node-uber');
+
+var uber = new Uber({
+  client_id: "O8C33qIolVTjUvIUc2VSKr53cGwEYJfg",
+  client_secret: CLIENT_SECRET,
+  server_token: "QkVbdUtpeEX3FII34_J3BIcwEYO1KoX5bxW6Mws9",
+  redirect_uri: 'REDIRECT URL',
+  name: 'SlackRide',
+  language: 'en_US', // optional, defaults to en_US
+  sandbox: true, // optional, defaults to false
+  proxy: 'PROXY URL' // optional, defaults to none
+})
