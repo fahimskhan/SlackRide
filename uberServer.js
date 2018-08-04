5f3e9cd58f3f2770b53b57dd29e64c11aa653b1f
belgren <belgren28@gmail.com>
2018-08-04 01:19:36 -0700

    changes to uberServer

--- uberServer.js
+++ uberServer.js
@@ -14,6 +14,8 @@ var uber = new Uber({
   sandbox: true, // optional, defaults to false
 })
 
+var products = [];
+
 
 var ACCESSTOKEN;
 
@@ -76,7 +78,12 @@ app.get('/api/products', function(request, response) {
     .then(function(res) {
       //what do we want to do with the product list??
       //returns an object with "products" key with value of an array of objects with the uber proucts
-      response.send(res);
+      if (res==null){
+        response.status(450).send('No Products Found');
+      } else {
+        products = res;
+        response.send(products);
+      }
     })
     .error(function(err) {
       console.error(err);
@@ -85,18 +92,51 @@ app.get('/api/products', function(request, response) {
   }
 });
 
-// app.get('/api/getproducts', function(request, response) {
-//   uber.products.getAllForAddressAsync('1455 Market St, San Francisco, CA 94103, US')
-// .then(function(res) {
-//    console.log(res);
-//
-//  })
-// .error(function(err) {
-//   console.error(err);
-// });
-// })
+app.post('/api/estimate', function (request, response) {
+//grab stuff from request . bodyParser
+//fetch(localhost.
+  console.log(request.body);
+
+  // let productId = request.query.productId;
+  // let startLong = request.query.startLong;
+  // let startLat = request.query.startLat;
+  // let endLong = request.query.endLong;
+  // let endLat= request.query.endLat;
+
+  //make request to uber
+  uber.requests.getEstimatesAsync({
+    "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
+    "start_latitude": startLat,
+    "start_longitude": startLong,
+    "end_latitude": endLat,
+    "end_longitude": endLong,
+  })
+  .then(function(res) {
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
 
 
+
+})
+
+app.post('/api/request', function(request, response)  {
+  //grab stuff from body parser
+
+  uber.requests.createAsync({
+  "fare_id": "d30e732b8bba22c9cdc10513ee86380087cb4a6f89e37ad21ba2a39f3a1ba960",
+  "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
+  "start_latitude": 37.761492,
+  "start_longitude": -122.423941,
+  "end_latitude": 37.775393,
+  "end_longitude": -122.417546
+})
+.then(function(res) { console.log(res); })
+.error(function(err) { console.error(err); });
+
+})
 
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
